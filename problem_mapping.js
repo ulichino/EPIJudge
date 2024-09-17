aee66f9f9aff2f0febaef2c98959c5cc008d421d
ulichino <74944775+ulichino@users.noreply.github.com>
2024-09-17 19:01:51 -0400

    matrix_enclosed_regions.py

--- problem_mapping.js
+++ problem_mapping.js
@@ -2494,7 +2494,7 @@ problem_mapping = {
                 "total": 51
             },
             "Python: matrix_enclosed_regions.py": {
-                "passed": 0,
+                "passed": 51,
                 "total": 51
             }
         },
