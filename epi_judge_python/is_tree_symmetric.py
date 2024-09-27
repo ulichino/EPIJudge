93078e1ee9b198c5fd3933d0666cdfe20203ba9b
ulichino <74944775+ulichino@users.noreply.github.com>
2024-09-27 13:14:45 -0400

    is_tree_symmetric.py

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -1,10 +1,16 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def is_symmetric_helper(node_a, node_b):
+    if not node_a and not node_b: # both are None
+        return True
+    if not node_a or not node_b: # one is None and the other isn't
+        return False
+    return node_a.data == node_b.data and is_symmetric_helper(node_a.left, node_b.right) and is_symmetric_helper(node_a.right, node_b.left)
+
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    return is_symmetric_helper(tree, tree)
 
 
 if __name__ == '__main__':
